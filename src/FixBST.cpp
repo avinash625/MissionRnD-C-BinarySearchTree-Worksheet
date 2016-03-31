b181d2e62b9c86b511e1e95355b755034d43092e
avinash625 <avinash625@missionrnd.com>
2016-04-01 00:26:14 +0530

    Submit Commit

--- src/FixBST.cpp
+++ src/FixBST.cpp
@@ -24,14 +24,16 @@ Nodes 1 and 20 need to be fixed here .
 */
 #include <stdio.h>
 #include <stdlib.h>
-
-
 struct node{
 	struct node * left;
 	int data;
 	struct node *right;
 };
-struct node *misplaced1 = NULL, *misplaced2 = NULL, *prev = NULL;
+struct node *misplaced1 = NULL, *misplaced2 = NULL, *prev = NULL,*parent = NULL,*child = NULL;
+void init(void)
+{
+	child = parent = misplaced1 = misplaced2 = prev = NULL;
+}
 void swap(struct node *first, struct node *second)
 {
 	int number = first->data;
@@ -52,17 +54,64 @@ void modified_inorder(struct node *root)
 			if (misplaced1 == NULL)
 				misplaced1 = prev;
 			else if (misplaced2 == NULL)
+			{
+				misplaced2 = root;
 				swap(misplaced1, root);
+			}
 		}
 		prev = root;
 		modified_inorder(root->right);
 	}
 }
+void get_parent(struct node *root)
+{
+	if (!root)
+		return;
+	if (root->data == misplaced1->data)
+		return;
+	if (root->left && root->left->data == misplaced1->data)
+		parent = root;
+	else if (root->right && root->right->data == misplaced1->data)
+		parent = root;
+	else
+	{
+		get_parent(root->left);
+		get_parent(root->right);
+	}
+}
+void get_child(struct node *root)
+{
+	if (misplaced1->left == NULL && misplaced1->right == NULL)
+		return;
+	if (misplaced1->right == NULL)
+		child = misplaced1->left;
+	else
+		child = misplaced1->right;
+}
 void fix_bst(struct node *root){
+	init();
 	if (!root)
 		return;
 	else
 	{
 		modified_inorder(root);
+		if (misplaced2 == NULL)
+		{
+			get_parent(root);
+			if (parent == NULL)
+			{
+				swap(misplaced1, misplaced1->right); return;
+			}
+			get_child(root);
+			if (child == NULL)
+			{
+				swap(misplaced1, parent); return;
+			}
+			else
+			{
+				if (parent->data > misplaced1->data)
+					swap(parent, misplaced1);
+			}
+		}
 	}
 }
